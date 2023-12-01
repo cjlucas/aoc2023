4e1b76474ab2945b3dbbd753e907457ca09b374c
Chris Lucas <chris@cjl.io>
2023-12-01 11:45:41 -0500

    cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,70 +1,68 @@
-const INPUT: &'static str = include_str!("../../inputs/day01.txt");
+const INPUT: &str = include_str!("../../inputs/day01.txt");
 
-fn part1(input: &str) -> u64 {
+fn solve(input: &str, words: Vec<(&str, u64)>) -> u64 {
     let mut sum = 0;
 
     for line in input.lines() {
-        let a = line
-            .chars()
-            .find(|ch| ch.is_numeric())
-            .unwrap()
-            .to_digit(10)
-            .unwrap();
-        let b = line
-            .chars()
-            .rev()
-            .find(|ch| ch.is_numeric())
-            .unwrap()
-            .to_digit(10)
-            .unwrap();
-        sum += (a * 10) + b;
-    }
-
-    sum.into()
-}
-
-fn part2(input: &str) -> u64 {
-    let words = vec![
-        ("1", 1u64),
-        ("2", 2),
-        ("3", 3),
-        ("4", 4),
-        ("5", 5),
-        ("6", 6),
-        ("7", 7),
-        ("8", 8),
-        ("9", 9),
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ];
-
-    let mut sum = 0;
-
-    for line in input.lines() {
-        let hits: Vec<_> = words
+        let a = words
             .iter()
             .filter_map(|(word, val)| line.find(word).map(|idx| (idx, val)))
-            .collect();
+            .min()
+            .unwrap();
 
-        let a = hits.iter().min().unwrap();
-
-        let hits: Vec<_> = words
+        let b = words
             .iter()
             .filter_map(|(word, val)| line.rfind(word).map(|idx| (idx, val)))
-            .collect();
+            .max()
+            .unwrap();
 
-        let b = hits.iter().max().unwrap();
         sum += (a.1 * 10) + b.1;
     }
 
-    sum.into()
+    sum
+}
+
+fn part1(input: &str) -> u64 {
+    solve(
+        input,
+        vec![
+            ("1", 1),
+            ("2", 2),
+            ("3", 3),
+            ("4", 4),
+            ("5", 5),
+            ("6", 6),
+            ("7", 7),
+            ("8", 8),
+            ("9", 9),
+        ],
+    )
+}
+
+fn part2(input: &str) -> u64 {
+    solve(
+        input,
+        vec![
+            ("1", 1),
+            ("2", 2),
+            ("3", 3),
+            ("4", 4),
+            ("5", 5),
+            ("6", 6),
+            ("7", 7),
+            ("8", 8),
+            ("9", 9),
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ],
+    )
 }
 
 fn main() {
@@ -78,8 +76,8 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
-        const INPUT: &'static str = include_str!("../../inputs/day01p1_example.txt");
-        assert_eq!(part1(INPUT), 142);
+        let input = include_str!("../../inputs/day01p1_example.txt");
+        assert_eq!(part1(input), 142);
     }
 
     #[test]
@@ -89,13 +87,13 @@ mod tests {
 
     #[test]
     fn test_part2_example() {
-        const INPUT: &'static str = include_str!("../../inputs/day01p2_example.txt");
-        assert_eq!(part2(INPUT), 281);
+        let input = include_str!("../../inputs/day01p2_example.txt");
+        assert_eq!(part2(input), 281);
     }
 
     #[test]
     fn test_part2() {
-        const INPUT: &'static str = include_str!("../../inputs/day01.txt");
-        assert_eq!(part2(INPUT), 53855);
+        let input = include_str!("../../inputs/day01.txt");
+        assert_eq!(part2(input), 53855);
     }
 }
