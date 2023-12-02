cbba108eb38111a6e79485281b5f505b6616dcd0
Chris Lucas <chris@cjl.io>
2023-12-02 00:19:04 -0500

    part2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -41,7 +41,37 @@ fn part1(input: &str) -> u64 {
 }
 
 fn part2(input: &str) -> u64 {
-    unimplemented!()
+    let mut power = 0;
+
+    for line in input.lines() {
+        let (_, sets) = line.split_once(':').unwrap();
+        let sets = sets.split(";").collect::<Vec<_>>();
+
+        let mut num_red = 0;
+        let mut num_green = 0;
+        let mut num_blue = 0;
+
+        for set in sets {
+            for cube in set.split(',') {
+                let (cnt, color) = cube.trim().split_once(' ').unwrap();
+                let cnt = u64::from_str_radix(cnt, 10).unwrap();
+
+                if color == "red" && cnt > num_red {
+                    num_red = cnt;
+                }
+                if color == "green" && cnt > num_green {
+                    num_green = cnt;
+                }
+                if color == "blue" && cnt > num_blue {
+                    num_blue = cnt;
+                }
+            }
+        }
+
+        power += num_red * num_green * num_blue;
+    }
+
+    power as u64
 }
 
 fn main() {
@@ -61,18 +91,18 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 0);
+        assert_eq!(part1(INPUT), 2331);
     }
 
-    // #[test]
-    // fn test_part2_example() {
-    //     let input = include_str!("../../inputs/day02p2_example.txt");
-    //     assert_eq!(part2(input), 0);
-    // }
+    #[test]
+    fn test_part2_example() {
+        let input = include_str!("../../inputs/day02_example.txt");
+        assert_eq!(part2(input), 2286);
+    }
 
-    // #[test]
-    // fn test_part2() {
-    //     let input = include_str!("../../inputs/day02.txt");
-    //     assert_eq!(part2(input), 0);
-    // }
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../../inputs/day02.txt");
+        assert_eq!(part2(input), 71585);
+    }
 }
