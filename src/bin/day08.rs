4dc415a866aab99892e81efaa8e38cd54c2063d1
Chris Lucas <chris@cjl.io>
2023-12-08 18:19:53 -0500

    --wip-- [skip ci]

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -63,11 +63,64 @@ fn part1(input: &str) -> u64 {
 }
 
 fn part2(input: &str) -> u64 {
-    unimplemented!()
+    let (directions, network) = input.split_once("\n\n").unwrap();
+    let directions = directions
+        .chars()
+        .map(|c| str::parse::<Direction>(&c.to_string()).unwrap())
+        .cycle();
+
+    let mut network_map = HashMap::<String, (String, String)>::new();
+
+    for line in network.lines() {
+        let (key, val) = line.split_once('=').unwrap();
+        let key = key.trim().to_string();
+        let (left, right) = val.split_once(',').unwrap();
+        let left = left.trim()[1..].to_string();
+        let right = right.trim()[..right.len() - 2].to_string();
+
+        network_map.insert(key, (left, right));
+    }
+
+    let mut cur_nodes: Vec<_> = network_map
+        .keys()
+        .filter(|node| node.ends_with('A'))
+        .cloned()
+        .collect();
+
+    dbg!(&network_map);
+
+    dbg!(cur_nodes.len());
+    dbg!(&cur_nodes);
+    // panic!();
+
+    let mut steps = 0;
+
+    for dir in directions {
+        if steps % 100000 == 0 {
+            println!("{}", steps);
+        }
+
+        for node in &mut cur_nodes {
+            let (left, right) = network_map.get(node).unwrap();
+
+            *node = match dir {
+                Direction::Left => left.to_string(),
+                Direction::Right => right.to_string(),
+            };
+        }
+
+        steps += 1;
+
+        if cur_nodes.iter().all(|node| node.ends_with('Z')) {
+            break;
+        }
+    }
+
+    steps
 }
 
 fn main() {
-    println!("part1: {}", part1(INPUT));
+    // println!("part1: {}", part1(INPUT));
     println!("part2: {}", part2(INPUT));
 }
 
@@ -92,11 +145,11 @@ mod tests {
         assert_eq!(part1(INPUT), 17873);
     }
 
-    // #[test]
-    // fn test_part2_example() {
-    //     let input = include_str!("../../inputs/day08_example.txt");
-    //     assert_eq!(part2(input), 2286);
-    // }
+    #[test]
+    fn test_part2_example() {
+        let input = include_str!("../../inputs/day08p2_example.txt");
+        assert_eq!(part2(input), 6);
+    }
 
     // #[test]
     // fn test_part2() {
