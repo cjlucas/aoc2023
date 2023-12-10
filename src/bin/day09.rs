493fb7c963574708cec606b9db3e010af7e9ad5e
Chris Lucas <chris@cjl.io>
2023-12-09 19:43:46 -0500

    part1

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -3,31 +3,36 @@ use aoc2023::prelude::*;
 const INPUT: &str = include_str!("../../inputs/day09.txt");
 
 fn part1(input: &str) -> i64 {
-    let mut foo: Vec<Vec<i64>> = vec![input
-        .lines()
-        .next()
-        .unwrap()
-        .split_whitespace()
-        .map(|n| str::parse::<i64>(n).unwrap())
-        .collect()];
+    let mut sums = vec![];
 
-    while !foo.last().unwrap().iter().all(|n| *n == 0) {
-        foo.push(
-            foo.last()
-                .unwrap()
-                .windows(2)
-                .map(|window| window[1] - window[0])
-                .collect(),
-        );
+    for line in input.lines() {
+        let mut foo: Vec<Vec<i64>> = vec![line
+            .split_whitespace()
+            .map(|n| str::parse::<i64>(n).unwrap())
+            .collect()];
+
+        while !foo.last().unwrap().iter().all(|n| *n == 0) {
+            foo.push(
+                foo.last()
+                    .unwrap()
+                    .windows(2)
+                    .map(|window| window[1] - window[0])
+                    .collect(),
+            );
+        }
+
+        for i in (1..foo.len()).rev() {
+            let curlast = *foo[i].iter().last().unwrap();
+            let prevlast = *foo[i - 1].iter().last().unwrap();
+            foo[i - 1].push(curlast + prevlast);
+        }
+
+        sums.push(*foo.iter().next().unwrap().iter().last().unwrap());
+
+        println!("foo: {:?}", foo);
     }
 
-    for i in (1..foo.len()).rev() {
-        foo[i - 1].push(foo[foo.len() - 1][-1] + foo[i][-1]);
-    }
-
-    println!("foo: {:?}", foo);
-
-    unimplemented!()
+    sums.iter().sum()
 }
 
 fn part2(input: &str) -> i64 {
@@ -46,7 +51,7 @@ mod tests {
     #[test]
     fn test_part1_example() {
         let input = include_str!("../../inputs/day09_example.txt");
-        assert_eq!(part1(input), 8);
+        assert_eq!(part1(input), 114);
     }
 
     // #[test]
